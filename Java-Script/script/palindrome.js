da0feccfdcbeb8aa03a44ee3def6db5890e0c57a
faisal-shh <meetshaikhfaisal@gmail.com>
2023-03-08 16:45:02 +0530

    done the palindrome code

--- Java-Script/script/palindrome.js
+++ Java-Script/script/palindrome.js
@@ -1,4 +1,4 @@
-var a=[3,1,3];
+/*var a=[3,1,3];
  for(var i=0; i<a.length-1; i++){
     for (var j=i+1; j<a.length-1; j++){
     if (a[i]==a[a.length-j]){
@@ -8,4 +8,14 @@ var a=[3,1,3];
         console.log('The given input is not a palindrome.')
     }
  }
+}*/
+
+var arr=[3,1,3,];
+var len=arr.length;
+var msg='It is a palindrome';
+for(var i=0; i < len/2; i++){
+    if(arr[i]!= arr[len-1-i]){
+        msg='It is not a palindrome';
+    }
 }
+console.log(`${arr}: ${msg}`);
